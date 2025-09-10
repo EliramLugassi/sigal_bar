3a7788423263e6bb335ddf45a0a0f48c79b0ee5f
EliramLugassi <eliram.lugassi@gmail.com>
2025-09-10 18:48:27 +0300

    Reapply "Use SQLAlchemy engine for pandas queries to avoid warning"
    This reverts commit c413874316faec7b251543d13fd438b905b1dba1.

--- modules/db_tools/db_connection.py
+++ modules/db_tools/db_connection.py
@@ -1,12 +1,27 @@
 """Database connection management."""
+import os
+
 import psycopg2
 import streamlit as st
-import os
+from sqlalchemy import create_engine
 
 from dotenv import load_dotenv
 
 
 load_dotenv()
+
+
+def _create_engine():
+    """Create a new SQLAlchemy engine for the database."""
+    return create_engine(
+        "postgresql+psycopg2://{}:{}@{}:{}/{}".format(
+            os.getenv("SUPABASE_DB_USER"),
+            os.getenv("SUPABASE_DB_PASSWORD"),
+            os.getenv("SUPABASE_DB_HOST"),
+            os.getenv("SUPABASE_DB_PORT"),
+            os.getenv("SUPABASE_DB_NAME"),
+        )
+    )
 
 
 def _create_connection():
@@ -18,6 +33,15 @@ def _create_connection():
         host=os.getenv("SUPABASE_DB_HOST"),
         port=os.getenv("SUPABASE_DB_PORT"),
     )
+
+
+def get_engine():
+    """Return a SQLAlchemy engine, creating it if necessary."""
+    engine = st.session_state.get("db_engine")
+    if engine is None:
+        engine = _create_engine()
+        st.session_state["db_engine"] = engine
+    return engine
 
 
 def get_connection():
